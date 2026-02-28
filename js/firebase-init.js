3b9ca15aa22551b3236ccb236c48e14b93b12f48
Jakub <23gprochazka@dgkralupy.eu>
2026-02-28 09:24:17 +0100

    demo prepare

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,11 +1,11 @@
 // Firebase config placeholder. Replace with your own config from Firebase Console.
 const firebaseConfig = {
-	apiKey: "AIzaSyA21RsfIPSgkmdULpoqFyimKBXfzx4G3_Y",
-	authDomain: "testapp1-a1eba.firebaseapp.com",
-	projectId: "testapp1-a1eba",
-	storageBucket: "testapp1-a1eba.firebasestorage.app",
-	messagingSenderId: "711086274272",
-	appId: "1:711086274272:web:df6d8974c03a5caae50285",
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
 };
 
 // Initialize Firebase
